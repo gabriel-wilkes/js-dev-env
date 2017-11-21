3eea0443fd94b6aba840d9d97af41a0ba03b1cb8
Gabriel <gabrielwilkes@gmail.com>
2017-11-21 19:25:00 +0900

    updates for heroku deployment

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
- return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+ return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://peaceful-cliffs-35962.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
